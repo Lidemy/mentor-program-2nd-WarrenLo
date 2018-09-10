5914c9c2a4a8a0f489d1adc924149d245706d65a
WarrenLo <warren810613@gmail.com>
2018-09-10 14:26:15 +0800

    hw5_version_2

--- homeworks/week1/hw5.js
+++ homeworks/week1/hw5.js
@@ -1,7 +1,30 @@
 function join(str, concatStr) {
-
+	var str_length = str.length;
+	var output = "";
+	for (var i = 0; i < 2*str_length-1; i++){
+		if (i % 2 ==0){
+			output += str[i/2];
+		}
+		if (i % 2 ==1){
+			output += concatStr;
+		}
+	}
+	return output;
 }
 
-function repeat(str, times) {
+var result_1 = join([1, 2, 3], '');
+var result_2 = join(["a", "b", "c"], "!");
+var result_3 = join(["a", 1, "b", 2, "c", 3], ',');
+console.log(result_1, result_2, result_3);
 
+function repeat(str,  times){
+	var output = "";
+	for ( var i = 1; i <= times; i++){
+		output += str;
+	}
+	return output;
 }
+
+var result_1 = repeat ("a", 5);
+var result_2 = repeat ("yoyo", 2);
+console.log(result_1, result_2);
